eb6d1ed39388ebcb662b3a9330dc00b05feaa74b
bartowwe <bartow.weiss@gmail.com>
2018-08-02 16:09:44 -0700

    Added a readme for setup

---
+++ ReadMe.txt
@@ -0,0 +1,12 @@
+Setting up the moreinfo module:
+1. Database:
+    a. Run sudo mongod in terminal
+    b. run mongo in terminal   
+        I. switch to yelp Database
+2. Run server in terminal
+    a. npm run start
+3. Run webpack in terminal
+    a. npm run setup
+
+*notes:
+1. Server runs on port 3005
--- proxy.js
+++ proxy.js
@@ -1,16 +1,94 @@
 const express = require('express');
-const bodyparser = require('body-parser');
+const parser = require('body-parser');
 const path = require('path');
+const request = require('request');
 
-const router = require('./router');
+const proxy = express();
 
-const server = express();
-const port = 3000;
+//this is the proxy port
+const port = 8000;
+proxy.use(parser.json());
+proxy.use(parser.urlencoded({extended: false}));
 
-server.use(bodyparser.json());
-server.use(bodyparser.urlencoded({ extended: true}));
-server.use(express.static(path.join(__dirname, '../client/dist')));
+proxy.use(express.static(path.join(__dirname, './')))
 
-server.use('/api', router);
+//Getting the photo view component bundle 
+proxy.get('/bundle.js/3001', (req, res) => {
+  request('http://localhost:3001/bundle.js', (err, response, body) => {
+    if(err) {
+      console.log(err)
+    } else {
+      res.status(200).send(body);
+    }
+  })
+})
 
-server.listen(port, () => console.log('server is connected'));
+//Getting the photo view component
+proxy.use('/main', (req, res) => {
+  request('http://localhost:3001/main', (err, response, body) => {
+    if(err) {
+      console.log(err)
+    }
+      res.status(200).send(body);
+  })
+})
+
+proxy.get('/bundle.js/3000', (req, res) => {
+  request('http://localhost:3000/bundle.js', (err, response, body) => {
+    if(err) {
+      console.log(err)
+    } else {
+      res.status(200).send(body);
+    }
+  })
+})
+
+proxy.use('/api/reviews', (req, res) => {
+  request('http://localhost:3000/api/reviews', (err, response, body) => {
+    if(err) {
+      console.log(err)
+    }
+      res.status(200).send(body);
+  })
+})
+
+proxy.get('/bundle.js/9001', (req, res) => {
+  request('http://localhost:9001/bundle.js', (err, response, body) => {
+    if(err) {
+      console.log(err)
+    } else {
+      res.status(200).send(body);
+    }
+  })
+})
+
+proxy.use('/api/yelp', (req, res) => {
+  request('http://localhost:9001/api/yelp', (err, response, body) => {
+    if(err) {
+      console.log(err)
+    }
+      res.status(200).send(body);
+  })
+})
+
+proxy.get('/bundle.js/1337', (req, res) => {
+  request('http://localhost:1337/bundle.js', (err, response, body) => {
+    if(err) {
+      console.log(err)
+    } else {
+      res.status(200).send(body);
+    }
+  })
+})
+
+proxy.use('/api/restaurantList', (req, res) => {
+  request('http://localhost:1337/api/restaurantList', (err, response, body) => {
+    if(err) {
+      console.log(err)
+    }
+      res.status(200).send(body);
+  })
+})
+
+
+proxy.listen(port, () => console.log(`Server is listening on port ${port}`));
